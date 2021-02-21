7153c2109b046c56adc9ddd329546bca463543f8
Michael Reed <michael@michaelreed.io>
2021-02-21 17:14:18 -0500

    Fix html-proofer errors locally

--- check_links.sh
+++ check_links.sh
@@ -3,4 +3,4 @@
 set -u
 set -e
 bundle exec jekyll build
-bundle exec htmlproofer ./_site
+bundle exec htmlproofer --assume-extension ./_site
--- index.md
+++ index.md
@@ -1,6 +1,7 @@
 ---
 layout: home
 title: Home
+permalink: /
 ---
 
 <!-- TODO The following text should go on a side panel -->
